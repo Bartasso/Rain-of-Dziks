36ccfcd7deac9df6bd0671e1356a5761243aa7ea
Bartasso <bartosiak.mich@gmail.com>
2023-01-14 01:06:08 +0100

    Created scriptable objects for enemiesa and basic enemy script. Assigned them to Mintos enemy and made a first basic setup of animations form him.

---
+++ Assets/Scripts/Scriptable Objects/EnemyScriptableObject.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Scriptable_Objects
+{
+    [CreateAssetMenu(fileName = "New Enemy", menuName = "ScriptableObjects/Enemy Scriptable Object")]
+
+    public class EnemyScriptableObject : ScriptableObject
+    {
+        public float health;
+        public float damage;
+        public bool isRanged;
+        public float attackRange;
+        public float engageDistance;
+        public float armour;
+        public float attackSpeed;
+        public AudioClip attackSound;
+        public GameObject projectile;
+        public float moveSpeed;
+        
+    }
+}
